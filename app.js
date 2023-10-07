570bc41982735760729ee02ac6600032de617427
sight <1453017105@qq.com>
2023-10-08 06:56:40 +0800

    演示版本回退至 v2.8.17

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION = '2.8.18-beta';
+const VERSION = '2.8.17';
 const layuicss = `https://unpkg.com/layui@${VERSION}/dist/css/layui.css`;
 const layuijs = `https://unpkg.com/layui@${VERSION}/dist/layui.js`;
 // const layuicss=`https://cdn.jsdelivr.net/gh/layui/layui@${VERSION}/dist/css/layui.css`;

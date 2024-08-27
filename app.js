41ed8a787d65c9e405c0eec3271e01d064689c69
sight <26325820+Sight-wcg@users.noreply.github.com>
2024-08-27 17:01:14 +0800

    fix: 修复 laydate 同时开启 shortcuts 和混合主题时日期面板布局异常的问题
    close #14

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const VERSION = '2.9.13';
+const VERSION = '2.9.16';
 const layuicss = `https://unpkg.com/layui@${VERSION}/dist/css/layui.css`;
 const layuijs = `https://unpkg.com/layui@${VERSION}/dist/layui.js`;
 // const layuicss=`https://cdn.jsdelivr.net/gh/layui/layui@${VERSION}/dist/css/layui.css`;
